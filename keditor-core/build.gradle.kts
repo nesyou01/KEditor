8ee72efda4b6fb0758794a9226a3dccda9df3c07
uness <lagmahraja@gmail.com>
2026-09-03 09:39:03 +0100

    Update maven info

--- keditor-core/build.gradle.kts
+++ keditor-core/build.gradle.kts
@@ -7,9 +7,6 @@ plugins {
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
-group = "io.github.kotlin"
-version = "1.0.0"
-
 kotlin {
     android {
         namespace = "dev.nesyou.keditor"
@@ -39,32 +36,4 @@ mavenPublishing {
     publishToMavenCentral()
 
     signAllPublications()
-
-    coordinates(group.toString(), "keditor", version.toString())
-
-    pom {
-        name = "KEditor"
-        description = "A library."
-        inceptionYear = "2024"
-        url = "https://github.com/XXX"
-        licenses {
-            license {
-                name = "XXX"
-                url = "YYY"
-                distribution = "ZZZ"
-            }
-        }
-        developers {
-            developer {
-                id = "XXX"
-                name = "YYY"
-                url = "ZZZ"
-            }
-        }
-        scm {
-            url = "XXX"
-            connection = "YYY"
-            developerConnection = "ZZZ"
-        }
-    }
 }
